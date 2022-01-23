22c56d77cf97837209879626a6188d17f37c05ea
Viktoriia Gorianska <viktoriia.gorianska@gmail.com>
2022-01-23 23:01:35 +0100

    added articles cards/teasers

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -11,6 +11,7 @@ import { red } from '@mui/material/colors';
 import { Star, Share } from '@mui/icons-material';
 import { Stack } from '@mui/material';
 import Pagination from '@mui/material/Pagination';
+import articles from "../articles.json";
 
 
 
@@ -19,25 +20,25 @@ export default function News() {
 
     return (
         <Stack spacing={2} sx={{ background: '#dde0e7', padding: 0, m: '70px 0' }}>
-            <Card sx={{ maxWidth: 345 }}>
+            {articles.map((article, i) => <Card sx={{ maxWidth: 345 }} key={i}>
                 <CardHeader
                     avatar={
                         <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            MP
+                            {article.author.split(" ").map((item) => item[0])}
                         </Avatar>
                     }
 
-                    title="Ideas for Kitchen Island Seating"
-                    subheader="By Mitchell Parker"
+                    title={article.title}
+                    subheader={article.author}
                 />
                 <CardMedia
                     component="img"
                     height="194"
-                    image="https://images.unsplash.com/photo-1565183928294-7063f23ce0f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                    image={article.img}
                 />
                 <CardContent>
                     <Typography variant="body2" color="text.secondary">
-                    Design and remodeling pros share details for creating a stylish and welcoming social hub
+                    {article.teaser}
                     </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
@@ -51,38 +52,9 @@ export default function News() {
                 </CardActions>
 
             </Card>
-            <Card sx={{ maxWidth: 345 }}>
-                <CardHeader
-                    avatar={
-                        <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                            MP
-                        </Avatar>
-                    }
-
-                    title="Ideas for Kitchen Island Seating"
-                    subheader="By Mitchell Parker"
-                />
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="https://images.unsplash.com/photo-1565183928294-7063f23ce0f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                />
-                <CardContent>
-                    <Typography variant="body2" color="text.secondary">
-                    Design and remodeling pros share details for creating a stylish and welcoming social hub
-                    </Typography>
-                </CardContent>
-                <CardActions disableSpacing>
-                    <IconButton aria-label="add to favorites">
-                        <Star />
-                    </IconButton>
-                    <IconButton aria-label="share">
-                        <Share />
-                    </IconButton>
-
-                </CardActions>
-
-            </Card>
+            )}
+            
+           
             <Pagination count={10} variant="outlined" shape="rounded" />
         </Stack>
     );
